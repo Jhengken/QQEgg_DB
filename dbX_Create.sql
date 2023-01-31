e844c420d049923fbae7a60df03ce464085a7641
重製 <hpu970390@gmail.com>
2023-01-31 22:33:57 +0800

    new vision

--- dbX_Create.sql
+++ dbX_Create.sql
@@ -23,7 +23,7 @@ use dbX
 drop table if exists Managers
 create table Managers(
 		ManagerID int identity(1,1),
-		Name nvarchar(50),
+		Name nvarchar(50) not null,
 		Email nvarchar(50),
 		Phone nvarchar(50),
 		Password nvarchar(50),
@@ -36,14 +36,14 @@ go
 drop table if exists Customers        
 Create table Customers(
 		CustomerID int identity(1,1),
-		Name nvarchar(50),
+		Name nvarchar(50) not null,
+		Sex bit,
 		Email nvarchar(50),
 		Phone nvarchar(50),
 		Password nvarchar(50),
-		Address nvarchar(50),
 		Birth datetime,
 		CreditCard nvarchar(50),
-		CreditPoints int,
+		CreditPoints int default 100,
 		BlackListed bit default 0,
 		constraint UQ_Customers_Email unique (Email),
 		constraint UQ_Customers_Phone unique (Phone),
@@ -55,18 +55,25 @@ go
 drop table if exists Suppliers
 Create table Suppliers(
 		SupplierID int identity(1,1),
-		Category nvarchar(50),
-		Name nvarchar(50),
+		Name nvarchar(50) not null,
 		Email nvarchar(50),
 		Phone nvarchar(50),
 		Password nvarchar(50),
 		Address nvarchar(50),
-		CreditPoints int,
-		BlackListed bit,
+		CreditPoints int default 100,
+		BlackListed bit default 0,
 		constraint UQ_Suppliers_Email unique (Email),
 		constraint UQ_Suppliers_Phone unique (Phone),
 		constraint PK_Suppliers primary key(SupplierID),
 		);
+go
+-----------------------Category OK
+
+drop table if exists Category
+Create table Category(
+		CategoryID int identity(1,1),
+		Name nvarchar(50) not null,
+		constraint PK_Category primary key (CategoryID asc))
 go
 -----------------------Products OK
 
@@ -74,16 +81,69 @@ drop table if exists Products
 Create table Products(
 		ProductID int identity(1,1),
 		SupplierID int,
-		Name nvarchar(50),
-		UnitPrice int,
-		Image nvarchar(50),
+		Name nvarchar(50) not null,
 		constraint PK_Products primary key (ProductID asc),
 		constraint FK_Products_Suppliers foreign key(SupplierID)
 		references dbX.dbo.Suppliers(SupplierID)
 		on delete cascade
 		on update cascade)
 go
------------------------COrders OK
+-----------------------PSite
+
+drop table if exists PSite
+Create table PSite(
+		SiteID int identity(1,1),
+		ProductID int,
+		Name nvarchar(50) not null,
+		Image nvarchar(50),
+		OpenTime nvarchar(50),
+		Latitude nvarchar(50),
+		Longitude nvarchar(50),
+		Address nvarchar(200),
+		Description nvarchar(200),
+		constraint PK_PSite primary key (SiteID asc),
+		constraint FK_PSite_Products foreign key(ProductID)
+		references dbX.dbo.Products(ProductID)
+		on delete cascade
+		on update cascade)
+go
+-----------------------PSiteRoom
+
+drop table if exists PSiteRoom
+Create table PSiteRoom(
+		RoomID int identity(1,1),
+		SiteID int,
+		CategoryID int,
+		HourPrice money,
+		DatePrice money,
+		Ping int,
+		Image nvarchar(50),
+		status bit default 0,
+		Description nvarchar(200),
+		constraint PK_PSiteRoom primary key (RoomID asc),
+		constraint FK_PSiteRoom_PSite foreign key(SiteID)
+		references dbX.dbo.PSite(SiteID)
+		on delete cascade
+		on update cascade,
+		constraint FK_PSiteRoom_Category foreign key(CategoryID)
+		references dbX.dbo.Category(CategoryID)
+		on delete cascade
+		on update cascade)
+go
+-----------------------Coupons OK
+
+drop table if exists Coupons
+Create table Coupons(
+		CouponID int identity(1,1),
+		Code nvarchar(50),
+		Discount money,            --折扣都用5折(0.5)、6折(0.6)、79折(0.79)
+		ExpiryDate datetime,
+		Quantity int,
+		Available bit default 1,
+		constraint UQ_Coupons_Code unique (Code),
+		constraint PK_Coupons primary key (CouponID asc))
+go
+-----------------------COrders
 
 drop table if exists COrders
 create table COrders(
@@ -95,10 +155,6 @@ create table COrders(
 		CancelDate datetime,
 		TakeDate datetime,
 		EndDate datetime,
-		Status nvarchar(50),
-		Quantity int,
-		Price money,
-		Code nvarchar(50),
 		constraint PK_COrders primary key(OrderID),
 		constraint FK_COrders_Customers foreign key(CustomerID)
 		references dbX.dbo.Customers(CustomerID)
@@ -107,37 +163,33 @@ create table COrders(
 		constraint FK_COrders_Products foreign key(ProductID)
 		references dbX.dbo.Products(ProductID)
 		on delete cascade
-		on update cascade
-		);
-go
-
------------------------PSite OK
-
-drop table if exists PSite
-Create table PSite(
-		SiteID int identity(1,1),
-		ProductID int,
-		Name nvarchar(50),
-		Address nvarchar(50),
-		Latitude nvarchar(50),
-		Longitude nvarchar(50),
-		UnitOrPing int,
-		UnitInStock int,
-		Image nvarchar(50),
-		Description nvarchar(500),
-		constraint PK_PSite primary key (SiteID asc),
-		constraint FK_PSite_Products foreign key(ProductID)
-		references dbX.dbo.Products(ProductID)
-		on delete cascade
 		on update cascade);
+go
+-----------------------COrderDetail
+
+drop table if exists COrderDetail
+Create table COrderDetail(
+		OrderID int identity(1,1),
+		CouponID int,
+		RoomID int,
+		Price money,
+		constraint PK_COrderDetail primary key (OrderID asc),
+		constraint FK_COrderDetail_PSiteRoom foreign key(RoomID)
+		references dbX.dbo.PSiteRoom(RoomID)
+		on delete cascade
+		on update cascade,
+		constraint FK_COrderDetail_Coupons foreign key(CouponID)
+		references dbX.dbo.Coupons(CouponID)
+		on delete cascade
+		on update cascade)
 go
 -----------------------Advertise OK
 
 drop table if exists Advertise
 create table Advertise(
 		AdvertiseID int identity(1,1),
-		Name nvarchar(50),
-		UnitPrice int,
+		Name nvarchar(50) not null,
+		DatePrice money,
 		constraint PK_Advertise primary key(AdvertiseID)
 		);
 go
@@ -151,6 +203,7 @@ create table AOrders(
 		OrderDate datetime default sysdatetime() not null,     --預設目前的系統時間
 		EndDate datetime,
 		Clicks int,
+		Price money,
 		constraint PK_AOrders primary key(AOrderID),
 		constraint FK_AOrders_Suppliers foreign key(SupplierID)
 		references dbx.dbo.Suppliers(SupplierID)
@@ -167,40 +220,30 @@ drop table if exists Evaluations
 create table Evaluations(
 		EvaluationID int identity(1,1),
 		CustomerID int,
-		ProductID int,
+		RoomID int,
 		Date datetime default sysdatetime(),     --預設目前的系統時間
-		Title nvarchar(100),
-		Description nvarchar(500),
-		Response nvarchar(500),
+		Title nvarchar(50),
+		Description nvarchar(200),
+		Response nvarchar(200),
 		Star int,
 		constraint PK_Evaluations primary key(EvaluationID),
 		constraint FK_Evaluations_Customers foreign key(CustomerID)
 		references dbX.dbo.Customers(CustomerID)
 		on delete cascade
 		on update cascade,
-		constraint FK_Evaluations_Products foreign key(ProductID)
-		references dbX.dbo.Products(ProductID)
+		constraint FK_Evaluations_PSiteRoom foreign key(RoomID)
+		references dbX.dbo.PSiteRoom(RoomID)
 		on delete cascade
 		on update cascade);
 go
------------------------Coupons OK
 
-drop table if exists Coupons
-Create table Coupons(
-		CouponID int identity(1,1),
-		Code nvarchar(50),
-		Discount money,            --折扣都用5折(0.5)、6折(0.6)、79折(0.79)
-		ExpiryDate datetime,
-		Quantity int,
-		Available bit,
-		constraint UQ_Coupons_Code unique (Code),
-		constraint PK_Coupons primary key (CouponID asc))
-go
------------------------CouponsLog OK
 
-drop table if exists CouponsLog
-Create table CouponsLog(
-		CouponID int identity(1,1),
-		OrderID int,
-		constraint PK_CouponsLog primary key (CouponID asc,OrderID))
-go
+
+----------------------------------[Suppliers] 資料表
+SET IDENTITY_INSERT [dbo].[Suppliers] ON 
+
+INSERT [dbo].[Suppliers] ([SupplierID], [Name], [Email], [Phone], [Password], [Address], [CreditPoints], [BlackListed]) VALUES (1, N'巴拉巴拉', N'bb@gmail.com', N'09111', N'b123', N'台南市南區', 100, 0)
+INSERT [dbo].[Suppliers] ([SupplierID], [Name], [Email], [Phone], [Password], [Address], [CreditPoints], [BlackListed]) VALUES (8, N'明明', N'mm@gmail.com', N'09222', N'm123', N'台南市東區', 100, 0)
+INSERT [dbo].[Suppliers] ([SupplierID], [Name], [Email], [Phone], [Password], [Address], [CreditPoints], [BlackListed]) VALUES (9, N'賣西瓜', N'mimi@gmail.com', N'09333', N'mi12', NULL, 100, 0)
+SET IDENTITY_INSERT [dbo].[Suppliers] OFF
+GO
