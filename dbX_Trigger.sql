cc0ca3dcd4968f57f658aaf88db6f997ffa86441
重製 <hpu970390@gmail.com>
2023-02-11 22:31:32 +0800

    新增trigger、修正FK第二版

---
+++ dbX_Trigger.sql
@@ -0,0 +1,74 @@
+use dbX
+--查看目前的trigger
+select * from sys.objects where type = 'TR'
+select * from sys.sysobjects where xtype = 'TR'
+
+--========tCOrders===================================
+
+------------------刪除指定orderID，刪除tCOrders外一並刪除tCOrderDetail
+drop trigger if exists tg_tCOrders_Delete
+go
+
+create trigger tg_tCOrders_Delete on tCOrders
+instead of Delete
+as
+begin
+	declare @OrderID int
+	select @OrderID = OrderID from deleted
+	delete from tCOrderDetail where OrderID=@OrderID
+	delete from tCOrders where OrderID=@OrderID
+	print '已刪除訂單OrderID : ' + str(@OrderID)
+end
+go
+
+--========tProducts===================================
+
+------------------刪除指定ProductID，刪除Product外一並刪除PSite
+drop trigger if exists tg_tProducts_Delete
+go
+
+create trigger tg_tProducts_Delete on tProducts
+instead of Delete
+as
+begin
+	declare @ProductID int, @SiteID int
+	select @ProductID=dp.ProductID, @SiteID=dps.SiteID from deleted as dp
+	join tPSite as dps on dp.ProductID=dps.ProductID
+
+	--delete join只能刪除一個
+	delete psr from tProducts as p
+	join tPSite as ps on p.ProductID=ps.ProductID
+	join tPSiteRoom as psr on ps.SiteID=psr.SiteID
+	where p.ProductID=@ProductID
+
+	print 'tProducts OK' + str(@@NESTLEVEL )
+	delete from tPSite where ProductID=@ProductID
+	delete from tProducts where ProductID=@ProductID
+end
+
+------------------刪除指定SiteID，刪除PSite外一並刪除PSiteRoom
+drop trigger if exists tg_tPSite_Delete
+go
+
+create trigger tg_tPSite_Delete on tPSite
+instead of Delete
+as
+IF @@NESTLEVEL >= 2
+	begin
+		declare @ProductID int
+		select @ProductID=ProductID from deleted
+		delete from tPSite where ProductID=@ProductID
+		--print 'NESTLEVEL' + str(@@NESTLEVEL )
+	end
+else
+	begin
+		declare @SiteID int
+		select @SiteID=SiteID from deleted
+		delete from tPSiteRoom where SiteID=@SiteID
+		delete from tPSite where SiteID=@SiteID
+		--print 'PSSSS' + str(@@NESTLEVEL )
+		--select * from tPSite where SiteID=@SiteID
+		--select * from tPSiteRoom where SiteID=@SiteID
+	end
+
+--=================================================
---
+++ 測試.sql
@@ -0,0 +1,22 @@
+use dbX
+
+  -----------------------------------COrder­q³æ
+  --select * from tCOrders
+  --select * from tCOrderDetail
+
+  --delete from tCOrders where OrderID=3
+  --delete from tCOrderDetail where RoomID=5
+
+  ----------------------------------Product
+ -- select * from tProducts as p
+ --join tPSite as ps on p.ProductID=ps.ProductID
+ --join tPSiteRoom as psr on ps.SiteID=psr.SiteID
+ -- where p.ProductID=1
+
+--select * from tProducts
+--select * from tPSite
+--select * from tPSiteRoom
+
+--delete from tProducts where ProductID=1    --siteID 2¡B5
+--delete from tPSite where SiteID=2
+--delete from tPSiteRoom where SiteID=2
