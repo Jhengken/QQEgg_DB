5392815dabd0ad394f7d34fdc2f5d6f12325d452
DESKTOP-38FFEEC\USER <enya19931025@gmail.com>
2023-04-10 11:50:42 +0800

    add data

--- dbX_Create.sql
+++ dbX_Create.sql
@@ -268,65 +268,22 @@ create table tEvaluations(
 go
 
 
-
 ----------------------------------[tSuppliers] 資料表
-SET IDENTITY_INSERT [dbo].[tSuppliers] ON 
-INSERT [dbo].[tSuppliers] ([SupplierID], [Name], [Email], [Phone], [Password], [Address], [CreditPoints], [BlackListed]) VALUES (1, N'巴拉巴拉', N'bb@gmail.com', N'09111', N'$2a$10$vJcS9ruHmONF7kh3jdbKTeIQCniYYHmv3xvaqcjAUvZ7LFRPPFI5y', N'台南市南區', 100, 0)
-INSERT [dbo].[tSuppliers] ([SupplierID], [Name], [Email], [Phone], [Password], [Address], [CreditPoints], [BlackListed]) VALUES (8, N'明明', N'mm@gmail.com', N'09222', N'$2a$10$XoqMCqqABSZ6mRDuy0XyPeTXCc07.hGR9KHAlddSYfSfnKVzq7fri', N'台南市東區', 100, 0)
-INSERT [dbo].[tSuppliers] ([SupplierID], [Name], [Email], [Phone], [Password], [Address], [CreditPoints], [BlackListed]) VALUES (9, N'賣西瓜', N'mimi@gmail.com', N'09333', N'$2a$10$3btEagcgJcfV1s9l4q0npOrlr1VHlYmoiONr6msz4wvldxb.4NSYy', NULL, 100, 0)
-SET IDENTITY_INSERT [dbo].[tSuppliers] OFF
+
 
 ----------------------------------[tCategory] 資料表
-SET IDENTITY_INSERT [dbo].[tCategory] ON 
-INSERT [dbo].[tCategory] ([CategoryID], [Name]) VALUES (1, N'多人辦公室')
-INSERT [dbo].[tCategory] ([CategoryID], [Name]) VALUES (2, N'會議室')
-INSERT [dbo].[tCategory] ([CategoryID], [Name]) VALUES (3, N'工作室')
-INSERT [dbo].[tCategory] ([CategoryID], [Name]) VALUES (4, N'展演空間')
-INSERT [dbo].[tCategory] ([CategoryID], [Name]) VALUES (5, N'廚房')
-SET IDENTITY_INSERT [dbo].[tCategory] OFF
+
 
 ----------------------------------[tETitle] 資料表
-SET IDENTITY_INSERT [dbo].[tETitle] ON 
-INSERT [dbo].[tETitle] ([TitleID], [TitleName]) VALUES (1, N'空間環境')
-INSERT [dbo].[tETitle] ([TitleID], [TitleName]) VALUES (2, N'設備相關')
-SET IDENTITY_INSERT [dbo].[tETitle] OFF
+
 
 ----------------------------------[tProducts] 資料表
-SET IDENTITY_INSERT [dbo].[tProducts] ON 
-INSERT [dbo].[tProducts] ([ProductID], [SupplierID], [Name]) VALUES (1, 1, N'巴拉巴拉的產品')
-INSERT [dbo].[tProducts] ([ProductID], [SupplierID], [Name]) VALUES (2, 8, N'明明的產品')
-INSERT [dbo].[tProducts] ([ProductID], [SupplierID], [Name]) VALUES (3, 9, N'西瓜的產品')
-SET IDENTITY_INSERT [dbo].[tProducts] OFF
+
 
 ----------------------------------[tPSite] 資料表
-SET IDENTITY_INSERT [dbo].[tPSite] ON 
-INSERT [dbo].[tPSite] ([SiteID], [ProductID], [Name], [Image], [OpenTime], [Latitude], [Longitude], [Address], [Description]) VALUES (2, 1, N'巴拉巴拉的第一站點', N'4.jpg', N'08:00~20:00', N'25.0402', N'121.5146', NULL, NULL)
-INSERT [dbo].[tPSite] ([SiteID], [ProductID], [Name], [Image], [OpenTime], [Latitude], [Longitude], [Address], [Description]) VALUES (5, 1, N'巴拉巴拉的第二站點', N'5.jpg', N'08:00~20:00', N'25.0264', N'121.5431', NULL, NULL)
-INSERT [dbo].[tPSite] ([SiteID], [ProductID], [Name], [Image], [OpenTime], [Latitude], [Longitude], [Address], [Description]) VALUES (6, 2, N'明明的第一站點', N'6.jpg', N'08:00~20:00', N'25.0358', N'121.5675', NULL, NULL)
-INSERT [dbo].[tPSite] ([SiteID], [ProductID], [Name], [Image], [OpenTime], [Latitude], [Longitude], [Address], [Description]) VALUES (7, 2, N'明明的第二戰點', N'7.jpg', N'08:00~20:00', N'24.1208', N'120.6676', NULL, NULL)
-INSERT [dbo].[tPSite] ([SiteID], [ProductID], [Name], [Image], [OpenTime], [Latitude], [Longitude], [Address], [Description]) VALUES (8, 3, N'西瓜的第一站點', N'1.jpg', N'08:00~20:00', N'24.1623', N'120.6415', N'南區', N'西瓜站點1描述')
-INSERT [dbo].[tPSite] ([SiteID], [ProductID], [Name], [Image], [OpenTime], [Latitude], [Longitude], [Address], [Description]) VALUES (10, 3, N'西瓜的第2站點', N'2.jpg', N'08:00~20:00', N'22.9923', N'120.2042', N'北區', N'西瓜站點2描述')
-INSERT [dbo].[tPSite] ([SiteID], [ProductID], [Name], [Image], [OpenTime], [Latitude], [Longitude], [Address], [Description]) VALUES (11, 3, N'西瓜的第3站點', N'3.jpg', N'08:00~20:00', N'23.0382', N'120.1827', N'東區', N'西瓜站點3描述')
-SET IDENTITY_INSERT [dbo].[tPSite] OFF
+
 
 ----------------------------------[tPSiteRoom] 資料表
-SET IDENTITY_INSERT [dbo].[tPSiteRoom] ON 
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (1, 2, 1, 10, 100, 15, N'1-1.jpg', 0, N'站點8房間1描述', NULL)
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (2, 2, 5, 15, 150, 150, N'5-1.jpg', 0, N'站點8房間1描述', NULL)
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (3, 5, 1, 11, 110, 33, N'1-2.jpg', 0, N'站點8房間1描述', NULL)
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (4, 5, 5, 12, 120, 25, N'5-2.jpg', 0, N'站點8房間1描述', NULL)
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (5, 6, 2, 13, 130, 20, N'2-1.jpg', 0, N'站點8房間1描述', NULL)
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (6, 6, 3, 14, 140, 15, N'3-1.jpg', 0, N'站點8房間1描述', NULL)
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (7, 8, 2, 14, 140, 10, N'2-2.jpg', 0, N'站點8房間1描述', N'123')
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (8, 8, 3, 13, 130, 15, N'3-2.jpg', 0, N'站點8房間2描述', N'456')
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (9, 8, 1, 12, 120, 17, N'1-3.jpg', 0, N'站點8房間3描述', N'789')
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (10, 10, 5, 12, 130, 16, N'5-3.jpg', 0, N'站點10房間1描述', N'111')
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (11, 11, 1, 10, 100, 9, N'1-1.jpg', 0, N'站點11房間1描述', N'222')
-INSERT [dbo].[tPSiteRoom] ([RoomID], [SiteID], [CategoryID], [HourPrice], [DatePrice], [Ping], [Image], [status], [Description], [RoomPassWord]) VALUES (12, 11, 4, 11, 121, 18, N'4-1.jpg', 0, N'站點11房間2描述', N'333')
-SET IDENTITY_INSERT [dbo].[tPSiteRoom] OFF
+
 
 ----------------------------------[tCustomers] 資料表
-SET IDENTITY_INSERT [dbo].[tCustomers] ON 
-INSERT [dbo].[tCustomers] ([CustomerID], [Name], [Sex], [Email], [Phone], [Password], [Birth], [CreditCard], [CreditPoints], [BlackListed]) VALUES (1, N'欸另', NULL, N'111@gmail.com', N'0911111111', N'$2a$10$1FJqseug2mCQE8h7FdbTIOdBIjq0.hSgBDDupSINPXKpVJCgbshiG', NULL, NULL, 100, 1)
-INSERT [dbo].[tCustomers] ([CustomerID], [Name], [Sex], [Email], [Phone], [Password], [Birth], [CreditCard], [CreditPoints], [BlackListed]) VALUES (2, N'阿奇', NULL, N'222@gmail.com', N'0922222222', N'$2a$10$psmPdpAmXTvN322JsIjY5.tVKjF3SWzN2GxOPp9b5FlT9Z0ZfeLSW', NULL, NULL, 100, 1)
-SET IDENTITY_INSERT [dbo].[tCustomers] OFF
